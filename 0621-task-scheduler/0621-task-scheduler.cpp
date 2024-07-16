6a13eb7a0eea3f94617d1a6653d58028ad926430
Shivam Kumar <111277773+codershiv2021@users.noreply.github.com>
2024-07-16 22:53:19 +0530

    Time: 75 ms (48.05%), Space: 41.9 MB (41.12%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<pair<int,int>>pq;
+        //contains tasks to be runn
+        queue<pair<int,int>>q;
+        //runn now put in queue to wait while cooldown
+        unordered_map<char,int>mp;
+        for (auto i: tasks){
+            mp[i]++;
+        }
+        int time = 0;
+        for (auto i: mp){
+            pq.push({i.second,0});
+        }
+        while(true){
+            //pq can be null
+
+            if(pq.empty() && q.empty()){
+                return time;
+            }
+
+            if (!q.empty()){
+                int rem_time = time- q.front().second;
+                if (rem_time>n){
+                    pq.push({q.front().first,rem_time});
+                    q.pop();
+                }
+            }
+            if (!pq.empty()){
+                if(pq.top().first>1){
+                    q.push({pq.top().first-1,time});
+                }
+                pq.pop();
+            }
+
+            time++;
+
+        }
+        return -1;
+    }
+};
