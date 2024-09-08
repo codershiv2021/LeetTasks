55eb330fc6f84a20af7d2a4d57fc3dab4cbc7583
Shivam Kumar <111277773+codershiv2021@users.noreply.github.com>
2024-09-08 13:27:53 +0530

    Time: 268 ms (100%), Space: 221.3 MB (100%) - LeetHub

---
+++ 3485-maximize-score-of-numbers-in-ranges/3485-maximize-score-of-numbers-in-ranges.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int ans = 0;
+    bool T(int mid, vector<int>&Start, int d) {
+        //[0,3,6]
+        vector<int>start= Start;
+int n = start.size(); int mini = INT_MAX; bool check = 1; //check kro
+        for (int i=1; i<n; i++){
+            if(start[i]-start[i-1]>=mid){
+                mini = min(mini, start[i]-start[i-1]);
+            }
+            else{
+                if (start[i]+d-start[i-1]<mid){
+                    check = 0;
+                }
+                else{
+                    start[i]= start[i-1]+mid;
+                    mini = min(mini, start[i]-start[i-1]);
+                }
+            }
+        }
+        if (check){
+            ans = mini;
+        }
+        return check;
+    }
+
+    int maxPossibleScore(vector<int>& start, int d) {
+        sort(start.begin(),start.end());
+        int n = start.size(); int high = 0;
+        for (int i=1; i<n; i++){
+            high= max(high, start[i]-start[i-1]);
+        }
+        int low = 0; high+=(d+1);  //6
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if (T(mid,start,d)){
+                low = mid+1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return ans;
+    }
+};
