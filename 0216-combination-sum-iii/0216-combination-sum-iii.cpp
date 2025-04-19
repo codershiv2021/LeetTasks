6845b4109711b247d1ce65737505f492764d8c8a
Shivam Kumar <111277773+codershiv2021@users.noreply.github.com>
2025-04-19 13:08:51 +0530

    Time: 0 ms (100%), Space: 8.8 MB (61.86%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void T(int i, int k, int n, vector<int>&v){
-        if (k==0 && n==0){
-            ans.push_back(v);
+    vector<vector<int>>v;
+void T(vector<int>&numbers, int k, int sum, int n, vector<int>&temp){
+        //k<0 - reurn .. 
+        if (k<0){
             return;
         }
-        if (k==0){
-            return;
-        }
-        for (int j=i; j<10; j++){
-            if (n>=j){
-                v.push_back(j);
-                T(j+1,k-1,n-j,v);
-                v.pop_back();
+        if (n==0){
+            if (sum==0 && k==0){
+                v.push_back(temp);
             }
+            return;
+        }
+        if (numbers[n-1]<=sum){
+            temp.push_back(numbers[n-1]);
+            T(numbers,k-1,sum-numbers[n-1],n-1,temp);
+            temp.pop_back();
+            T(numbers,k,sum,n-1,temp);
+        }
+        else{
+            T(numbers,k,sum,n-1,temp);
         }
         return;
     }
-
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>v;
-        T(1,k,n,v);
-        return ans;
+        vector<int>numbers;
+        for (int i=1; i<=9; i++){
+            numbers.push_back(i);
+        }
+        vector<int>temp;
+        T(numbers,k,n,9,temp);
+        return v;
     }
 };
